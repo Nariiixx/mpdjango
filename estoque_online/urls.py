bcea684b01c0e0be4e6e1149e4719023979f4c31
Nariiixx <iranilsonmarinho.com@gmail.com>
2025-02-18 15:30:40 -0400

    alterações feitas  no path de urls

--- estoque_online/urls.py
+++ estoque_online/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.phome, name='phome'),
-    path('/cadastro/', views.pcadastro, name='pcadastro'),
-    path('/atualizar/', views.atualizar, name='atualizar'),
-    path('/localizar/', views.localizar, name='localizar'),
-    path('/lista_produtos/', views.lista_de_produto, name='lista_de_produto'),
-    path('/relatorio/', views.relatorio, name='relatorio')
+    path('cadastro/', views.pcadastro, name='pcadastro'),
+    path('atualizar/', views.atualizar, name='atualizar'),
+    path('localizar/', views.localizar, name='localizar'),
+    path('lista_produtos/', views.lista_de_produto, name='lista_de_produto'),
+    path('relatorio/', views.relatorio, name='relatorio')
 ]
