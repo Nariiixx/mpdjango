906a2f5862ab701738b0f7457bc9079bad17b837
Nariiixx <iranilsonmarinho.com@gmail.com>
2025-02-18 01:43:55 -0400

    alteração no urls

--- djangobasico/urls.py
+++ djangobasico/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('est/', include('estoque_online.urls'))
+    path('/admin/', admin.site.urls),
+    path('', include('estoque_online.urls'))
 ]
--- estoque_online/urls.py
+++ estoque_online/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.phome, name='phome'),
-    path('home/cadastro/', views.pcadastro, name='pcadastro'),
-    path('home/atualizar/', views.atualizar, name='atualizar'),
-    path('home/localizar/', views.localizar, name='localizar'),
-    path('home/lista_produtos/', views.lista_de_produto, name='lista_de_produto'),
-    path('home/relatorio/', views.relatorio, name='relatorio')
+    path('', views.phome, name='phome'),
+    path('/cadastro/', views.pcadastro, name='pcadastro'),
+    path('/atualizar/', views.atualizar, name='atualizar'),
+    path('/localizar/', views.localizar, name='localizar'),
+    path('/lista_produtos/', views.lista_de_produto, name='lista_de_produto'),
+    path('/relatorio/', views.relatorio, name='relatorio')
 ]
